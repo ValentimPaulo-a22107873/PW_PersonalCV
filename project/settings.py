4fff57d6eae78dd9eda646da98b15b8082af5c1c
Valentim <valentimspaulo@gmail.com>
2023-06-14 15:34:27 +0100

    ...

--- project/settings.py
+++ project/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,5 +130,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # see https://help.pythonanywhere.com/pages/DjangoStaticFiles for more info
 MEDIA_ROOT = '/home/ValentimPaulo3/main/media'
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/home/ValentimPaulo3/main/static'
 STATIC_URL = '/static/'
